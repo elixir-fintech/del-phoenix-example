16d00569eba158ce6d5b3eaa143f9e38f0509088
Christian Sommerauer <csommerauer@outfarm.com>
2025-08-07 18:54:59 +0100

    add DoubleEntryLedger database

--- config/test.exs
+++ config/test.exs
@@ -13,6 +13,16 @@ config :del_example, DelExample.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
+config :double_entry_ledger, DoubleEntryLedger.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "del_example_test#{System.get_env("MIX_TEST_PARTITION")}",
+  stacktrace: true,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: System.schedulers_online() * 2
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :del_example, DelExampleWeb.Endpoint,

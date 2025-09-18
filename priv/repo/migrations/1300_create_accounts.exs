e8d84782fcf7fddc229b83a58122f4fef516fd69
Christian Sommerauer <csommerauer@outfarm.com>
2025-09-18 16:15:13 +0100

    unique index issue

--- priv/repo/migrations/1300_create_accounts.exs
+++ priv/repo/migrations/1300_create_accounts.exs
@@ -14,21 +14,16 @@ defmodule DoubleEntryLedger.Repo.Migrations.CreateAccounts do
       add :pending, :map, default: %{}
       add :available, :integer, null: false, default: 0
       add :allowed_negative, :boolean, default: true
-
-      add :instance_id, references(:instances, on_delete: :restrict, type: :binary_id),
-        null: false
-
-      # Optimistic locking
-      add :lock_version, :integer, default: 1
+      add :instance_id, references(:instances, on_delete: :restrict, type: :binary_id), null: false
+      add :lock_version, :integer, default: 1 # Optimistic locking
 
       timestamps(type: :utc_datetime_usec)
     end
 
     create index(:accounts, [:instance_id], prefix: "double_entry_ledger")
-
     create unique_index(:accounts, [:instance_id, :name],
-             prefix: "double_entry_ledger",
-             name: "unique_instance_name"
-           )
+      prefix: "double_entry_ledger",
+      name: "unique_instance_name"
+    )
   end
 end
